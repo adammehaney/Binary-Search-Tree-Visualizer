73dbfef741e41cbab0e75b72b2fb33e773db611a
Adam Mehaney <59457164+adammehaney@users.noreply.github.com>
2024-04-07 03:23:57 -0600

    created draw tree, info cards, inserting into tree

--- src/typescript/BST.ts
+++ src/typescript/BST.ts
@@ -59,13 +59,23 @@ const canvas = document.getElementById("main-canvas") as HTMLCanvasElement;
 		}
 	}
 
-	function drawNode(ctx: CanvasRenderingContext2D, position: Vector2D) {
+	function drawNode(ctx: CanvasRenderingContext2D, rootValue: number, position: Vector2D) {
+		ctx.fillStyle = "#000000ff";
+		ctx.textAlign = "center";
+		ctx.textBaseline = "middle";
+		ctx.font = "20px system-ui, sans-serif"
+		ctx.fillText(rootValue.toString(), position.x, position.y);
+
+		ctx.fillStyle = "white";
+
 		ctx.beginPath();
-		ctx.arc(position.x, position.y, 10, 0, Math.PI * 2, true);
+		ctx.arc(position.x, position.y, 20, 0, Math.PI * 2, true);
 
 		ctx.fill();
 	}
 	function connectPoints(ctx: CanvasRenderingContext2D, position1: Vector2D, position2: Vector2D) {
+		ctx.lineWidth = 5;
+
 		ctx.beginPath();
 		ctx.moveTo(position1.x, position1.y);
 		ctx.lineTo(position2.x, position2.y);
@@ -135,53 +145,103 @@ const canvas = document.getElementById("main-canvas") as HTMLCanvasElement;
 
 		return result
 	}
-
-	if (canvas.getContext) {
-		const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
-
-		var scale = 1;
-		canvas.style.width = '50vw';
-		canvas.style.height = '50vh';
-		canvas.width = canvas.offsetWidth;
-		canvas.height = canvas.offsetHeight;
-
-		ctx.fillStyle = "white";
-		ctx.strokeStyle = "#fca311ff";
-
-		// const points = [
-		// 	[canvas.offsetWidth / 2, 10],
-		// 	[canvas.offsetWidth / 4, 100],
-		// 	[canvas.offsetWidth / 1.5, 100],
-		// 	[canvas.offsetWidth / 8, 200],
-		// 	[canvas.offsetWidth / 2.5, 200],
-		// 	[canvas.offsetWidth / 1.2, 200],
-		// 	[canvas.offsetWidth / 16, 300],
-		// 	[canvas.offsetWidth / 4.5, 300],
-		// ];
-
-		// points.forEach((point) => {
-		// 	drawLine(ctx, point[0], point[1]);
-		// });
-
-		// points.forEach((point) => {
-		// 	drawNode(ctx, point[0], point[1]);
-		// });
-
-		var tree = new BinaryTree(1)
-		tree.insertLeft(2)
-		tree.insertRight(7)
-		tree.getLeft().insertLeft(3)
-		tree.getLeft().insertRight(6)
-		tree.getLeft().getLeft().insertLeft(4)
-		tree.getLeft().getLeft().insertRight(5)
-		tree.getRight().insertLeft(8)
-		tree.getRight().insertRight(9)
-
-		let preOrderTree = postOrder(tree);
-		for (let i = 0; i < preOrderTree.length; i++) {
-			console.log(preOrderTree[i]);
+	function insert(tree: BinaryTree, value: number) {
+		if (value > tree.getRoot()) {
+			if (tree.getLeft() === null && tree.getRight() === null) {
+				tree.insertRight(value);
+			} else {
+				insert(tree.getRight(), value)
+			}
+		} else {
+			if (tree.getLeft() === null && tree.getRight() === null) {
+				tree.insertLeft(value);
+			} else {
+				insert(tree.getLeft(), value)
+			}
 		}
-
-		console.log("MIN", minRoot(tree));
-		console.log("MAX", maxRoot(tree));
 	}
+    function drawTree(ctx: CanvasRenderingContext2D, tree: BinaryTree, canvasWidth: number, scale: number, i=0, rootPosition: any=null) {          
+        if (tree) {
+            if (rootPosition === null) {
+                rootPosition = new Vector2D(canvasWidth / 2, 50);
+            }
+
+			drawNode(ctx, tree.getRoot(), rootPosition);
+
+            if (tree.getLeft()) {
+                let leftPosition = new Vector2D(rootPosition.x - (100 / (i + 1)) * scale, rootPosition.y + 50);
+
+				drawNode(ctx, tree.getLeft().getRoot(), leftPosition);
+				connectPoints(ctx, rootPosition, leftPosition);
+                drawTree(ctx, tree.getLeft(), canvasWidth, scale, i + 1, leftPosition);
+            }
+            if (tree.getRight()) {
+                let rightPosition = new Vector2D(rootPosition.x + (100 / (i + 1)) * scale, rootPosition.y + 50);
+
+				drawNode(ctx, tree.getRight().getRoot(), rightPosition);
+				connectPoints(ctx, rootPosition, rightPosition);
+                drawTree(ctx, tree.getRight(), canvasWidth, scale, i + 1, rightPosition);
+            }
+        }
+    }
+	function updateTraversals(tree: BinaryTree) {
+		var preOrderElement = document.getElementById('preorder-list') as HTMLElement;
+		preOrderElement.innerText = preOrder(tree).toString();
+
+		var inOrderElement = document.getElementById('inorder-list') as HTMLElement;
+		inOrderElement.innerText = inOrder(tree).toString();
+
+		var postOrderElement = document.getElementById('postorder-list') as HTMLElement;
+		postOrderElement.innerText = postOrder(tree).toString();
+	}
+
+	function buildCanvas() {
+		if (canvas.getContext) {
+			const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+	
+			var scale = 1;
+			canvas.style.width = '50vw';
+			canvas.style.height = '50vh';
+			canvas.width = canvas.offsetWidth;
+			canvas.height = canvas.offsetHeight;
+	
+			ctx.globalCompositeOperation='destination-over';
+	
+			ctx.fillStyle = "white";
+			ctx.strokeStyle = "#fca311ff";
+	
+			var tree = new BinaryTree(1);
+			tree.insertLeft(2);
+			tree.insertRight(7);
+			tree.getLeft().insertLeft(3);
+			tree.getLeft().insertRight(6);
+			tree.getLeft().getLeft().insertLeft(4);
+			tree.getLeft().getLeft().insertRight(5);
+			tree.getRight().insertLeft(8);
+			tree.getRight().insertRight(9);
+	
+			updateTraversals(tree);
+	
+			console.log("MIN", minRoot(tree));
+			console.log("MAX", maxRoot(tree));
+	
+			drawTree(ctx, tree, canvas.width, scale);
+
+			var number = document.getElementById('insert-form-input') as HTMLInputElement;
+			var numberForm = document.getElementById('insert-form') as HTMLElement;
+
+			// if (numberForm.addEventListener){
+				
+			// }
+			numberForm.addEventListener("submit", function (){
+				console.log(number.value);
+				insert(tree, parseInt(number.value))
+				drawTree(ctx, tree, canvas.width, scale);
+				updateTraversals(tree);
+			}, false);
+		}
+	}
+
+	buildCanvas();
+
+	window.addEventListener("resize", buildCanvas);
