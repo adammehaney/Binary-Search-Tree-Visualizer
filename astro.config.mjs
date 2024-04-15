0544249abad0b20881db75e5e6b9f2e79e00aa44
Adam Mehaney <59457164+adammehaney@users.noreply.github.com>
2024-04-15 02:59:04 -0600

    added sitemap, robots.txt, fixed github icon load

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
 import icon from "astro-icon";
-
 import svelte from "@astrojs/svelte";
+import sitemap from "@astrojs/sitemap";
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon(), svelte()]
+  // site: 'https://adammehaney.github.io',
+  // base: 'Binary-Search-Tree-Visualizer',
+  integrations: [icon(), svelte(), sitemap(), robotsTxt()]
 });
